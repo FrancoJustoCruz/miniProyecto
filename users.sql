626aa3911afe9d515144e23fba8ca91864b5e540
justocruzfranco <justocruzfranco@gmail.com>
2024-03-11 07:53:48 -0500

    segundo commit

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-03-2024 a las 06:01:36
+-- Tiempo de generación: 11-03-2024 a las 13:27:02
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -44,16 +44,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nombres`, `apellidos`, `direccion`, `correo_electronico`, `dni`, `edad`, `fecha_creacion`, `telefono`) VALUES
-(40459406, 'Lethia', 'Souch', '5 Vera Trail', 'lsouch4@huffingtonpost.com', '258-27-6619', 96, '2008-05-29 13:02:42', '840-922-3612'),
-(92820145, 'Debee', 'Peckett', '83 Cherokee Crossing', 'dpeckett3@squarespace.com', '664-16-6083', 81, '2014-06-22 12:10:12', '574-903-5691'),
-(419185282, 'Juliet', 'Bamb', '911 Fair Oaks Plaza', 'jbamb2@ycombinator.com', '165-46-7196', 40, '2011-08-14 16:43:18', '634-640-4340'),
-(424549881, 'Millard', 'Beare', '87 Dwight Trail', 'mbeare5@bandcamp.com', '318-48-8283', 37, '2009-06-24 04:03:45', '815-391-6091'),
-(667637028, 'Waldemar', 'Grayshon', '33360 Gerald Drive', 'wgrayshon6@dailymotion.com', '408-47-7658', 12, '2015-05-30 13:20:55', '237-793-9168'),
-(1115856327, 'Bernard', 'Tiner', '573 Rowland Plaza', 'btiner8@blogtalkradio.com', '116-53-6380', 137, '2002-06-28 22:58:55', '943-245-0691'),
-(1167919201, 'Erhart', 'Spalls', '547 Algoma Way', 'espalls1@dropbox.com', '151-79-8931', 42, '2012-01-15 16:52:55', '965-582-0731'),
-(1407830619, 'Leslie', 'Heffernon', '6513 Moulton Plaza', 'lheffernon9@nasa.gov', '356-49-8560', 129, '2008-03-29 19:55:41', '206-248-1552'),
-(1708389464, 'Nixie', 'Burbridge', '8914 Melby Park', 'nburbridge7@wikispaces.com', '738-36-8257', 3, '2005-08-31 13:57:15', '503-753-7960'),
-(2087856608, 'Wilhelmina', 'Korous', '802 Warbler Park', 'wkorous0@pinterest.com', '231-02-3918', 133, '2018-06-01 14:03:48', '553-937-2530');
+(1, 'Lethia', 'Souch', '5 Vera Trail', 'lsouch4@huffingtonpost.com', '258-27-6619', 96, '2008-05-29 13:02:42', '840-922-3612'),
+(2, 'Debee', 'Peckett', '83 Cherokee Crossing', 'dpeckett3@squarespace.com', '664-16-6083', 81, '2014-06-22 12:10:12', '574-903-5691'),
+(3, 'Juliet', 'Bamb', '911 Fair Oaks Plaza', 'jbamb2@ycombinator.com', '165-46-7196', 40, '2011-08-14 16:43:18', '634-640-4340'),
+(4, 'Millard', 'Beare', '87 Dwight Trail', 'mbeare5@bandcamp.com', '318-48-8283', 37, '2009-06-24 04:03:45', '815-391-6091'),
+(5, 'Waldemar', 'Grayshon', '33360 Gerald Drive', 'wgrayshon6@dailymotion.com', '408-47-7658', 12, '2015-05-30 13:20:55', '237-793-9168'),
+(6, 'Bernard', 'Tiner', '573 Rowland Plaza', 'btiner8@blogtalkradio.com', '116-53-6380', 137, '2002-06-28 22:58:55', '943-245-0691'),
+(7, 'Erhart', 'Spalls', '547 Algoma Way', 'espalls1@dropbox.com', '151-79-8931', 42, '2012-01-15 16:52:55', '965-582-0731'),
+(8, 'Leslie', 'Heffernon', '6513 Moulton Plaza', 'lheffernon9@nasa.gov', '356-49-8560', 129, '2008-03-29 19:55:41', '206-248-1552'),
+(9, 'Nixie', 'Burbridge', '8914 Melby Park', 'nburbridge7@wikispaces.com', '738-36-8257', 3, '2005-08-31 13:57:15', '503-753-7960'),
+(10, 'Wilhelmina', 'Korous', '802 Warbler Park', 'wkorous0@pinterest.com', '231-02-3918', 133, '2018-06-01 14:03:48', '553-937-2530');
 
 --
 -- Índices para tablas volcadas
@@ -73,7 +73,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2087856609;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
